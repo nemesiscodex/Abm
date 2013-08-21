b9d7efca92118e9f6b14effd5e451da1635f0921
Alexander <Alexander@Alexander-PC>
2013-08-20 23:22:39 -0430

    productos

---
+++ src/java/entidades/productos.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author Alexander
+ */
+@Stateless
+@Path("/productos")
+public class productos {
+    ArrayList<producto> productos = new ArrayList<producto>();
+    int secuencia =1;
+    
+    @GET
+    public String listarProductos() {
+        if (this.productos.isEmpty()) {
+            return "No existen productos registrados";
+        } else {
+            Gson objetogson = new Gson();
+            return objetogson.toJson(productos);
+        }
+    }
+    
+}
+
+class producto{
+    int id;
+    double costo;
+    String nombre;
+    String Descripcion;
+    
+    public producto(int id, double costo, String nombre, String Descripcion) {
+        this.id = id;
+        this.costo = costo;
+        this.nombre = nombre;
+        this.Descripcion = Descripcion;
+    }
+    
+}
