df0f34edc9ee80d5c582f472e20931311b4748d0
Alexander <Alexander@Alexander-PC>
2013-08-20 21:26:31 -0430

    inicial

---
+++ src/java/NewClass.java
@@ -0,0 +1,17 @@
+
+import javax.ws.rs.Path;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alexander
+ */
+@Path("/NewClass")
+
+public class NewClass {
+    
+}
